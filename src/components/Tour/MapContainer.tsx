2756b27fd0ef9436ba7cc73e115886db0de9a49a
Theu <info@matteocontini.com>
2019-08-25 14:42:18 +0200

    set up shared folder for constants

--- src/components/Tour/MapContainer.tsx
+++ src/components/Tour/MapContainer.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+import { TEST } from '../../Shared/constants';
+
 const WrapperMap = styled.div`
     border:1px solid black;
 `;
@@ -10,7 +12,7 @@ export const MapContainer: React.FC<{}> = (props) => {
     
     return (
         <WrapperMap>
-            In this tour we will visit the four conrers around my building
+            In this tour we will visit the four conrers around my building {TEST}
         </WrapperMap>
     );
 }
