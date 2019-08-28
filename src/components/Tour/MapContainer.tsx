2f3119398faf17f141b521b3e8205f6262bbb6eb
Theu <info@matteocontini.com>
2019-08-28 21:08:56 +0200

    the call is appening

--- src/components/Tour/MapContainer.tsx
+++ src/components/Tour/MapContainer.tsx
@@ -1,22 +1,68 @@
 import * as React from 'react';
+// import 'rxjs/add/operator/map';
 import styled from 'styled-components';
 
-import { 
-    TEMPORARY_KEY as apiKey,
-    SECOND_TEST } from 'Shared/constants';
+import { TEMPORARY_KEY as GOOGLE_API_KEY } from 'Shared/constants';
+
+type MapContainerProps = {
+  map?: object
+};
 
 const WrapperMap = styled.div`
     border:1px solid black;
+    height: 20px;
 `;
 
 
-export const MapContainer: React.FC<{}> = (props) => {
-    console.log('SECOND_TEST', SECOND_TEST)
-    return (
-        <WrapperMap>
-            In this tour we will visit the four conrers around my building
-        </WrapperMap>
-    );
-}
+export class MapContainer extends React.PureComponent<MapContainerProps> {
+    // private map = React.createRef<HTMLDivElement>()
+    // private current = true
+    // get myValue (): boolean { return this.current }
+    
+    state = {
+      mapIsReady: false
+    }
+
+    componentDidMount() {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`;
+      script.async = true;
+      script.defer = true;
+      script.addEventListener('load', () => {
+        this.setState({ mapIsReady: true });
+      });
+
+      document.body.appendChild(script);
+    }
+
+    componentDidUpdate() {
+      if (this.state.mapIsReady) {
+        this.map = new google.maps.Map(
+          document.getElementById("map"),
+          {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 12,
+            // mapTypeId: 'roadmap',
+          }
+        )
+      }
+    }
+
+    handleClick = () => {
+        console.log('eccomi')
+    }
+    
+    render() {
+        return (
+            <div>
+                <WrapperMap>
+                    <div id="map" onClick={this.handleClick}>
+                        coraggio
+                    </div>
+                </WrapperMap>
+            </div>
+        );
+    }
+};
 
 export default MapContainer;
